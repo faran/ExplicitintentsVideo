e8846ae4fc76a1204c5683bc39c15e75dd27366a
fkha0003 <faran.khan@synchronoss.com>
2019-01-07 14:55:03 +0000

    Initial commit

---
+++ app/src/main/java/com/pg/fk/explicitintentsvideo/Activity2.java
@@ -0,0 +1,23 @@
+package com.pg.fk.explicitintentsvideo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Activity2 extends AppCompatActivity {
+
+    TextView tvWelcome;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+        tvWelcome = findViewById(R.id.tvWelcome);
+
+        String name = getIntent().getStringExtra("name");
+
+        tvWelcome.setText(name + ", Welcome to Activity 2");
+
+    }
+}
---
+++ app/src/main/java/com/pg/fk/explicitintentsvideo/Activity3.java
@@ -0,0 +1,49 @@
+package com.pg.fk.explicitintentsvideo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Activity3 extends AppCompatActivity {
+
+    EditText etSurname;
+    Button btnSubmit, btnCancel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_3);
+
+        etSurname = findViewById(R.id.etSurName);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        btnCancel = findViewById(R.id.btnCancel);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (etSurname.getText().toString().isEmpty()) {
+                    Toast.makeText(Activity3.this, "Please enter all fields", Toast.LENGTH_SHORT).show();
+                }else{
+                    String surname = etSurname.getText().toString().trim();
+                    Intent intent = new Intent();
+                    intent.putExtra("surnamedata", surname);
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setResult(RESULT_CANCELED);
+                finish();
+            }
+        });
+
+    }
+}
